88fe590e5971ba09f26ebde52754efeaaf1b7e3c
Vardaan Aggarwal <98269336+VardaanAggarwal@users.noreply.github.com>
2023-03-03 13:54:10 +0530

    adding the save tab function

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-let inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
-let ulEl = document.getElementById("ul-el")
 let myLeads = []
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value) 
-    renderLeads()
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+const leadsfromlocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsfromlocalStorage) {
+    myLeads = leadsfromlocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
-function renderLeads(){
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+function render(Leads){
     let listItems = ""
-    for(let count = 0; count < myLeads.length; count++){
-        listItems += `<li><a href="${myLeads[count]}" target="_blank">${myLeads[count]}</a></li>`
+    for(let count = 0; count < Leads.length; count++){
+        listItems += `<li><a href="${Leads[count]}" target="_blank">${Leads[count]}</a></li>`
     }
     ulEl.innerHTML = listItems
     inputEl.value = ''
