30a94c41d9e818a1ba955ba0afa4ad1db7f18f71
Vardaan Aggarwal <98269336+VardaanAggarwal@users.noreply.github.com>
2023-04-05 23:37:23 +0530

    indentation fix

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
-const leadsfromlocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+const leadsfromlocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-if(leadsfromlocalStorage) {
-    myLeads = leadsfromlocalStorage
-    render(myLeads)
+if (leadsfromlocalStorage) {
+  myLeads = leadsfromlocalStorage;
+  render(myLeads);
 }
 
-tabBtn.addEventListener("click", function(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    })
-})
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
-deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
-})
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
 
-function render(Leads){
-    let listItems = ""
-    for(let count = 0; count < Leads.length; count++){
-        listItems += `<li><a href="${Leads[count]}" target="_blank">${Leads[count]}</a></li>`
-    }
-    ulEl.innerHTML = listItems
-    inputEl.value = ''
+function render(Leads) {
+  let listItems = "";
+  for (let count = 0; count < Leads.length; count++) {
+    listItems += `<li><a href="${Leads[count]}" target="_blank">${Leads[count]}</a></li>`;
+  }
+  ulEl.innerHTML = listItems;
+  inputEl.value = "";
 }
